b79c347d5521dbfcfc98323e2a4f0c8bd90d2b49
dotzaki <dev@dotzaki.me>
2024-01-20 14:33:17 +1300

    fix: wrong playbook path forgot to update when fixing the init script

--- home/run_onchange_bAnsible.sh
+++ home/run_onchange_bAnsible.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Basically whenever we change anything in the ansible playbook chezmoi apply will run the playbook
-# ./slay.yml hash: {{ include "./slay.yml" | sha256sum }}
+# ~/.config/ansible/slay.yml hash: {{ include "~/.config/ansible/slay.yml" | sha256sum }}
 
 if command -v ansible-playbook &>/dev/null; then
-	ansible-playbook {{ joinPath .chezmoi.sourceDir "./slay.yml" | quote }} --ask-become-pass
+	ansible-playbook {{ joinPath .chezmoi.sourceDir "~/.config/ansible/slay.yml" | quote }} --ask-become-pass
 fi
